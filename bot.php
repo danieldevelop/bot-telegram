04cc14ce9e3be05b2f7d71ad4a51186aa61eb154
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-23 17:36:14 -0400

    script of test to bot telegram

---
+++ bot.php
@@ -0,0 +1,47 @@
+<?php
+
+$token = "6024389131:AAEFZNhv9-A95f0eQNezYs7rJ8gNwlonmaw"; // token (id u contraseña) de acceso para el bot
+$website = "https://api.telegram.org/bot".$token;
+
+$input = file_get_contents('php://input'); // recibe la informacion de la url, en formato json
+$update = json_decode($update, TRUE); // decodifica la informacion recibida
+
+$chatId = $update["message"]["chat"]["id"]; 
+$message = $update["message"]["text"];
+
+switch ($message) {
+    case '/start':
+        $response = "Bienvenido al bot de prueba";
+        sendMessage($chatId, $response);
+        break;
+
+    case strtolower('Hola bot'):
+        $response = "Hola, ¿como estas?";
+        sendMessage($chatId, $response);
+        break;
+
+    case strtolower('Que tal bot'):
+        $response = "Muy bien, ¿y tu?";
+        sendMessage($chatId, $response);
+        break;
+
+    case strtolower('Como me ira en la sumativa'):
+        $response = 'te ira super bien si realizas los ejercicios';
+        sendMessage($chatId, $response);
+        break;
+
+    case '!Adios bot':
+        $response = "Hasta luego";
+        sendMessage($chatId, $response);
+        break;
+
+    default:
+        $response = "No te entiendo";
+        sendMessage($chatId, $response);
+}
+
+function sendMessage($chatId, $response){
+    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&parse_mode=HTML&text=".urlencode($response);
+    file_get_contents($url);
+}
+
