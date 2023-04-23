260e32faf3eedd8abd6af88d00b55cc004c4585a
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-23 18:21:39 -0400

    se corrige sintaxis de string

--- bot.php
+++ bot.php
@@ -42,7 +42,8 @@ switch ($message) {
 }
 
 function sendMessage($chatId, $response){
-    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&parse_mode=HTML&text=".urlencode($response);
+    $url = $GLOBALS['website']."/sendMessage?chat_id=".$chatId."&parse_mode=HTML&text=".urlencode($response);
     file_get_contents($url);
 }
 
+?>
